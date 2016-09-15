5653678bb52c8de091296e9200992b1608a5f772
Shantanu Kumar <shantanu0412kumar@gmail.com>
2016-09-15 22:26:37 +0530

    help

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Http\Controllers;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+     public function getDashboard()
+    {
+        return view('dashboard');
+    }
+    public function postSignUp(Request $request )
+    {
+         $this->validate($request,[
+             'email'=>'required|email|unique:users',
+             'username'=>'required|max:120',
+             'password'=>'required|min:4'
+         ]);
+          $email = $request['email'];
+          $name = $request['username'];
+          $password = bcrypt($request['password']);
+          
+          $user = new User;
+          $user->email=$email;
+          $user->name=$name;
+          $user->password=$password;
+
+          $user->save();
+          Auth::login($user);
+          return redirect()->route('dashboard');
+          
+    }
+    public function postSignIn(Request $request)
+    {
+         $this->validate($request,[
+             'email'=>'required',
+             'password'=>'required'
+         ]);
+        if(Auth::attempt(['email' => $request['email'],'password' => $request['password']])){
+            return redirect()->route('dashboard');
+        }
+        return redirect()->back();
+    }
+}
